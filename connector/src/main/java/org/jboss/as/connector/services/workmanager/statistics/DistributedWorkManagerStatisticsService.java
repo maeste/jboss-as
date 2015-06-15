wildfly/wildfly

WFLY-4429 Refactor JCA subsystems ---> Use of PASSIVE services

connector/src/main/java/org/jboss/as/connector/services/workmanager/statistics/DistributedWorkManagerStatisticsService.java
@@ -1,0 +1,140 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.connector.services.workmanager.statistics;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+import org.jboss.as.connector.dynamicresource.ClearWorkManagerStatisticsHandler;
+import org.jboss.as.connector.subsystems.resourceadapters.CommonAttributes;
+import org.jboss.as.connector.subsystems.resourceadapters.Constants;
+import org.jboss.as.connector.subsystems.resourceadapters.WorkManagerRuntimeAttributeReadHandler;
+import org.jboss.as.connector.subsystems.resourceadapters.WorkManagerRuntimeAttributeWriteHandler;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ResourceBuilder;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.descriptions.OverrideDescriptionProvider;
+import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.jca.core.api.workmanager.DistributedWorkManager;
+import org.jboss.msc.inject.Injector;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+import org.jboss.msc.value.InjectedValue;
+
+public class DistributedWorkManagerStatisticsService implements Service<ManagementResourceRegistration> {
+
+    private final ManagementResourceRegistration overrideRegistration;
+    private final boolean statsEnabled;
+
+    protected final InjectedValue<DistributedWorkManager> distributedWorkManager = new InjectedValue<>();
+
+
+    /**
+     * create an instance *
+     */
+    public DistributedWorkManagerStatisticsService(final ManagementResourceRegistration registration,
+                                                   final String name,
+                                                   final boolean statsEnabled) {
+        super();
+        if (registration.isAllowsOverride()) {
+            overrideRegistration = registration.registerOverrideModel(name, new OverrideDescriptionProvider() {
+                @Override
+                public Map<String, ModelNode> getAttributeOverrideDescriptions(Locale locale) {
+                    return Collections.emptyMap();
+                }
+
+                @Override
+                public Map<String, ModelNode> getChildTypeOverrideDescriptions(Locale locale) {
+                    return Collections.emptyMap();
+                }
+
+            });
+        } else {
+            overrideRegistration = registration;
+        }
+        this.statsEnabled = statsEnabled;
+
+    }
+
+
+    @Override
+    public void start(StartContext context) throws StartException {
+        synchronized (this) {
+            DistributedWorkManager dwm = distributedWorkManager.getValue();
+            dwm.setDistributedStatisticsEnabled(statsEnabled);
+
+            if (dwm.getStatistics() != null) {
+                PathElement peDistributedWm = PathElement.pathElement(Constants.STATISTICS_NAME, "distributed");
+
+                ResourceBuilder resourceBuilder = ResourceBuilder.Factory.create(peDistributedWm,
+                        new StandardResourceDescriptionResolver(Constants.STATISTICS_NAME + "." + Constants.WORKMANAGER_NAME, CommonAttributes.RESOURCE_NAME, CommonAttributes.class.getClassLoader()));
+
+                ManagementResourceRegistration dwmSubRegistration = overrideRegistration.registerSubModel(resourceBuilder.build());
+
+                OperationStepHandler metricHandler = new WorkManagerRuntimeAttributeReadHandler(dwm, dwm.getDistributedStatistics(), false);
+                for (SimpleAttributeDefinition metric : Constants.WORKMANAGER_METRICS) {
+                    dwmSubRegistration.registerMetric(metric, metricHandler);
+                }
+
+                OperationStepHandler readHandler = new WorkManagerRuntimeAttributeReadHandler(dwm, dwm.getDistributedStatistics(), false);
+                OperationStepHandler writeHandler = new WorkManagerRuntimeAttributeWriteHandler(dwm, false, Constants.DISTRIBUTED_WORKMANAGER_RW_ATTRIBUTES);
+
+                for (SimpleAttributeDefinition attribute : Constants.DISTRIBUTED_WORKMANAGER_RW_ATTRIBUTES) {
+                    dwmSubRegistration.registerReadWriteAttribute(attribute, readHandler, writeHandler);
+                }
+
+
+                dwmSubRegistration.registerOperationHandler(ClearWorkManagerStatisticsHandler.DEFINITION, new ClearWorkManagerStatisticsHandler(dwm));
+
+            }
+        }
+    }
+
+
+    @Override
+    public void stop(StopContext context) {
+
+        PathElement peDistributedWm = PathElement.pathElement(Constants.STATISTICS_NAME, "distributed");
+        if (overrideRegistration.getSubModel(PathAddress.pathAddress(peDistributedWm)) != null)
+            overrideRegistration.unregisterSubModel(peDistributedWm);
+
+    }
+
+
+    @Override
+    public ManagementResourceRegistration getValue() throws IllegalStateException, IllegalArgumentException {
+        return overrideRegistration;
+    }
+
+
+    public Injector<DistributedWorkManager> getDistributedWorkManagerInjector() {
+        return distributedWorkManager;
+    }
+}

connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/RaActivate.java
@@ -22,16 +22,15 @@
 
 package org.jboss.as.connector.subsystems.resourceadapters;
 
+import static org.jboss.as.connector.subsystems.resourceadapters.Constants.ARCHIVE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceName;
-
-import static org.jboss.as.connector.subsystems.resourceadapters.Constants.ARCHIVE;
-import static org.jboss.as.connector.subsystems.resourceadapters.Constants.STATISTICS_ENABLED;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 
 /**
  * Operation handler responsible for disabling an existing data-source.
@@ -47,7 +46,6 @@ public class RaActivate implements OperationStepHandler {
         final String idName = PathAddress.pathAddress(address).getLastElement().getValue();
         ModelNode model = context.readResource(PathAddress.EMPTY_ADDRESS).getModel();
         final String archiveName = ARCHIVE.resolveModelAttribute(context, model).asString();
-        final boolean statsEnabled = STATISTICS_ENABLED.resolveModelAttribute(context, model).asBoolean();
 
         if (context.isNormalServer()) {
             context.addStep(new OperationStepHandler() {
@@ -56,7 +54,7 @@ public class RaActivate implements OperationStepHandler {
                     ServiceName restartedServiceName = RaOperationUtil.restartIfPresent(context, archiveName, idName);
 
                     if (restartedServiceName == null) {
-                        RaOperationUtil.activate(context, idName, archiveName, statsEnabled);
+                        RaOperationUtil.activate(context, idName, archiveName);
                     }
                     context.completeStep(new OperationContext.RollbackHandler() {
                         @Override

connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/RaOperationUtil.java
@@ -334,7 +334,7 @@ public class RaOperationUtil {
 
     }
 
-    public static void activate(OperationContext context, String raName, String archiveName, boolean statsEnabled) throws OperationFailedException {
+    public static void activate(OperationContext context, String raName, String archiveName) throws OperationFailedException {
         ServiceRegistry registry = context.getServiceRegistry(true);
         ServiceController<?> inactiveRaController = registry.getService(ConnectorServices.INACTIVE_RESOURCE_ADAPTER_SERVICE.append(archiveName));
 
@@ -351,7 +351,7 @@ public class RaOperationUtil {
         final ServiceController<?> RaxmlController = registry.getService(ServiceName.of(ConnectorServices.RA_SERVICE, raName));
         Activation raxml = (Activation) RaxmlController.getValue();
         RaServicesFactory.createDeploymentService(inactive.getRegistration(), inactive.getConnectorXmlDescriptor(), inactive.getModule(), inactive.getServiceTarget(),
-                raName, inactive.getDeploymentUnitServiceName(), inactive.getDeployment(), raxml, inactive.getResource(), statsEnabled);
+                raName, inactive.getDeploymentUnitServiceName(), inactive.getDeployment(), raxml, inactive.getResource());
     }
 
     public static ServiceName installRaServices(OperationContext context, String name, ModifiableResourceAdapter resourceAdapter, final List<ServiceController<?>> newControllers) {
@@ -459,7 +459,7 @@ public class RaOperationUtil {
                 final ServiceName deployerServiceName = ConnectorServices.RESOURCE_ADAPTER_DEPLOYER_SERVICE_PREFIX.append(connectorXmlDescriptor.getDeploymentName());
                 final ServiceController<?> deployerService = context.getServiceRegistry(true).getService(deployerServiceName);
                 if (deployerService == null) {
-                    ServiceBuilder builder = ParsedRaDeploymentProcessor.process(connectorXmlDescriptor, ironJacamarXmlDescriptor, module.getClassLoader(), serviceTarget, annotationIndexes, RAR_MODULE.append(name));
+                    ServiceBuilder builder = ParsedRaDeploymentProcessor.process(connectorXmlDescriptor, ironJacamarXmlDescriptor, module.getClassLoader(), serviceTarget, annotationIndexes, RAR_MODULE.append(name), null, null);
                     newControllers.add(builder.addDependency(raServiceName).setInitialMode(ServiceController.Mode.ACTIVE).install());
                 }
                 String rarName = resourceAdapter.getArchive();

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/statistics/ResourceAdapterStatisticsTestCase.java
@@ -212,11 +212,11 @@ public class ResourceAdapterStatisticsTestCase extends JcaStatisticsBase {
     @Override
     public ModelNode translateFromConnectionToStatistics(ModelNode connectionNode) {
         ModelNode statNode = new ModelNode();
-        statNode.add(DEPLOYMENT, connectionNode.get(1).get("resource-adapter").asString());
+        //statNode.add(DEPLOYMENT, connectionNode.get(1).get("resource-adapter").asString());
         statNode.add(SUBSYSTEM, "resource-adapters");
-        statNode.add("statistics", "statistics");
         statNode.add(connectionNode.get(1));
         statNode.add("connection-definitions", connectionNode.get(2).get("connection-definitions").asString());
+        statNode.add("statistics", "local");
         return statNode;
     }
 }

maeste/jboss-as

connector/src/main/java/org/jboss/as/connector/services/workmanager/statistics/DistributedWorkManagerStatisticsService.java
@@ -1,0 +1,140 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.connector.services.workmanager.statistics;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+import org.jboss.as.connector.dynamicresource.ClearWorkManagerStatisticsHandler;
+import org.jboss.as.connector.subsystems.resourceadapters.CommonAttributes;
+import org.jboss.as.connector.subsystems.resourceadapters.Constants;
+import org.jboss.as.connector.subsystems.resourceadapters.WorkManagerRuntimeAttributeReadHandler;
+import org.jboss.as.connector.subsystems.resourceadapters.WorkManagerRuntimeAttributeWriteHandler;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ResourceBuilder;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.descriptions.OverrideDescriptionProvider;
+import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.jca.core.api.workmanager.DistributedWorkManager;
+import org.jboss.msc.inject.Injector;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+import org.jboss.msc.value.InjectedValue;
+
+public class DistributedWorkManagerStatisticsService implements Service<ManagementResourceRegistration> {
+
+    private final ManagementResourceRegistration overrideRegistration;
+    private final boolean statsEnabled;
+
+    protected final InjectedValue<DistributedWorkManager> distributedWorkManager = new InjectedValue<>();
+
+
+    /**
+     * create an instance *
+     */
+    public DistributedWorkManagerStatisticsService(final ManagementResourceRegistration registration,
+                                                   final String name,
+                                                   final boolean statsEnabled) {
+        super();
+        if (registration.isAllowsOverride()) {
+            overrideRegistration = registration.registerOverrideModel(name, new OverrideDescriptionProvider() {
+                @Override
+                public Map<String, ModelNode> getAttributeOverrideDescriptions(Locale locale) {
+                    return Collections.emptyMap();
+                }
+
+                @Override
+                public Map<String, ModelNode> getChildTypeOverrideDescriptions(Locale locale) {
+                    return Collections.emptyMap();
+                }
+
+            });
+        } else {
+            overrideRegistration = registration;
+        }
+        this.statsEnabled = statsEnabled;
+
+    }
+
+
+    @Override
+    public void start(StartContext context) throws StartException {
+        synchronized (this) {
+            DistributedWorkManager dwm = distributedWorkManager.getValue();
+            dwm.setDistributedStatisticsEnabled(statsEnabled);
+
+            if (dwm.getDistributedStatistics() != null) {
+                PathElement peDistributedWm = PathElement.pathElement(Constants.STATISTICS_NAME, "distributed");
+
+                ResourceBuilder resourceBuilder = ResourceBuilder.Factory.create(peDistributedWm,
+                        new StandardResourceDescriptionResolver(Constants.STATISTICS_NAME + "." + Constants.WORKMANAGER_NAME, CommonAttributes.RESOURCE_NAME, CommonAttributes.class.getClassLoader()));
+
+                ManagementResourceRegistration dwmSubRegistration = overrideRegistration.registerSubModel(resourceBuilder.build());
+
+                OperationStepHandler metricHandler = new WorkManagerRuntimeAttributeReadHandler(dwm, dwm.getDistributedStatistics(), false);
+                for (SimpleAttributeDefinition metric : Constants.WORKMANAGER_METRICS) {
+                    dwmSubRegistration.registerMetric(metric, metricHandler);
+                }
+
+                OperationStepHandler readHandler = new WorkManagerRuntimeAttributeReadHandler(dwm, dwm.getDistributedStatistics(), false);
+                OperationStepHandler writeHandler = new WorkManagerRuntimeAttributeWriteHandler(dwm, false, Constants.DISTRIBUTED_WORKMANAGER_RW_ATTRIBUTES);
+
+                for (SimpleAttributeDefinition attribute : Constants.DISTRIBUTED_WORKMANAGER_RW_ATTRIBUTES) {
+                    dwmSubRegistration.registerReadWriteAttribute(attribute, readHandler, writeHandler);
+                }
+
+
+                dwmSubRegistration.registerOperationHandler(ClearWorkManagerStatisticsHandler.DEFINITION, new ClearWorkManagerStatisticsHandler(dwm));
+
+            }
+        }
+    }
+
+
+    @Override
+    public void stop(StopContext context) {
+
+        PathElement peDistributedWm = PathElement.pathElement(Constants.STATISTICS_NAME, "distributed");
+        if (overrideRegistration.getSubModel(PathAddress.pathAddress(peDistributedWm)) != null)
+            overrideRegistration.unregisterSubModel(peDistributedWm);
+
+    }
+
+
+    @Override
+    public ManagementResourceRegistration getValue() throws IllegalStateException, IllegalArgumentException {
+        return overrideRegistration;
+    }
+
+
+    public Injector<DistributedWorkManager> getDistributedWorkManagerInjector() {
+        return distributedWorkManager;
+    }
+}

connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/RaActivate.java
@@ -22,6 +22,10 @@
 
 package org.jboss.as.connector.subsystems.resourceadapters;
 
+import static org.jboss.as.connector.subsystems.resourceadapters.Constants.ARCHIVE;
+import static org.jboss.as.connector.subsystems.resourceadapters.Constants.MODULE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
 import org.jboss.as.connector.logging.ConnectorLogger;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
@@ -29,11 +33,6 @@ import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceName;
-
-import static org.jboss.as.connector.subsystems.resourceadapters.Constants.ARCHIVE;
-import static org.jboss.as.connector.subsystems.resourceadapters.Constants.MODULE;
-import static org.jboss.as.connector.subsystems.resourceadapters.Constants.STATISTICS_ENABLED;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 
 /**
  * Operation handler responsible for disabling an existing data-source.
@@ -57,7 +56,6 @@ public class RaActivate implements OperationStepHandler {
         } else {
             archiveOrModuleName = model.get(MODULE.getName()).asString();
         }
-        final boolean statsEnabled = STATISTICS_ENABLED.resolveModelAttribute(context, model).asBoolean();
 
         if (context.isNormalServer()) {
             context.addStep(new OperationStepHandler() {
@@ -66,7 +64,7 @@ public class RaActivate implements OperationStepHandler {
                     ServiceName restartedServiceName = RaOperationUtil.restartIfPresent(context, archiveOrModuleName, idName);
 
                     if (restartedServiceName == null) {
-                        RaOperationUtil.activate(context, idName, archiveOrModuleName, statsEnabled);
+                        RaOperationUtil.activate(context, idName, archiveOrModuleName);
                     }
                     context.completeStep(new OperationContext.RollbackHandler() {
                         @Override

connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/RaOperationUtil.java
@@ -334,7 +334,7 @@ public class RaOperationUtil {
 
     }
 
-    public static void activate(OperationContext context, String raName, String archiveName, boolean statsEnabled) throws OperationFailedException {
+    public static void activate(OperationContext context, String raName, String archiveName) throws OperationFailedException {
         ServiceRegistry registry = context.getServiceRegistry(true);
         ServiceController<?> inactiveRaController = registry.getService(ConnectorServices.INACTIVE_RESOURCE_ADAPTER_SERVICE.append(archiveName));
 
@@ -351,7 +351,7 @@ public class RaOperationUtil {
         final ServiceController<?> RaxmlController = registry.getService(ServiceName.of(ConnectorServices.RA_SERVICE, raName));
         Activation raxml = (Activation) RaxmlController.getValue();
         RaServicesFactory.createDeploymentService(inactive.getRegistration(), inactive.getConnectorXmlDescriptor(), inactive.getModule(), inactive.getServiceTarget(),
-                archiveName, inactive.getDeploymentUnitServiceName(), inactive.getDeployment(), raxml, inactive.getResource(), statsEnabled);
+                archiveName, inactive.getDeploymentUnitServiceName(), inactive.getDeployment(), raxml, inactive.getResource());
     }
 
     public static ServiceName installRaServices(OperationContext context, String name, ModifiableResourceAdapter resourceAdapter, final List<ServiceController<?>> newControllers) {
@@ -459,7 +459,7 @@ public class RaOperationUtil {
                 final ServiceName deployerServiceName = ConnectorServices.RESOURCE_ADAPTER_DEPLOYER_SERVICE_PREFIX.append(connectorXmlDescriptor.getDeploymentName());
                 final ServiceController<?> deployerService = context.getServiceRegistry(true).getService(deployerServiceName);
                 if (deployerService == null) {
-                    ServiceBuilder builder = ParsedRaDeploymentProcessor.process(connectorXmlDescriptor, ironJacamarXmlDescriptor, module.getClassLoader(), serviceTarget, annotationIndexes, RAR_MODULE.append(name));
+                    ServiceBuilder builder = ParsedRaDeploymentProcessor.process(connectorXmlDescriptor, ironJacamarXmlDescriptor, module.getClassLoader(), serviceTarget, annotationIndexes, RAR_MODULE.append(name), null, null);
                     newControllers.add(builder.addDependency(raServiceName).setInitialMode(ServiceController.Mode.ACTIVE).install());
                 }
                 String rarName = resourceAdapter.getArchive();

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/statistics/ResourceAdapterStatisticsTestCase.java
@@ -212,11 +212,11 @@ public class ResourceAdapterStatisticsTestCase extends JcaStatisticsBase {
     @Override
     public ModelNode translateFromConnectionToStatistics(ModelNode connectionNode) {
         ModelNode statNode = new ModelNode();
-        statNode.add(DEPLOYMENT, connectionNode.get(1).get("resource-adapter").asString());
+        //statNode.add(DEPLOYMENT, connectionNode.get(1).get("resource-adapter").asString());
         statNode.add(SUBSYSTEM, "resource-adapters");
-        statNode.add("statistics", "statistics");
         statNode.add(connectionNode.get(1));
         statNode.add("connection-definitions", connectionNode.get(2).get("connection-definitions").asString());
+        statNode.add("statistics", "pool");
         return statNode;
     }
 }